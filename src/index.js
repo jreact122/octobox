31f3df9df401e47f335c69b6837192ef64f7f8f1
jreact122 <129377811+jreact122@users.noreply.github.com>
2023-04-01 12:03:41 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "../src/assets/sb-admin-2.css";
+import "../src/assets/js/demo/chart-area-demo";
+import "../src/assets/js/demo/chart-bar-demo";
+import "../src/assets/js/demo/chart-pie-demo";
+import "../src/assets/js/demo/datatables-demo";
+import "../src/assets/js/demo/chart-area-demo";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
